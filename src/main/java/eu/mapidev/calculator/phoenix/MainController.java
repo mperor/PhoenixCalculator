7f37cf5bb63b9a598962e6a78dd178e1fde7bf1f
Mark Pi <mpietryga93@gmail.com>
2022-03-28 21:13:13 +0200

    Add binding for clean and number buttons

--- src/main/java/eu/mapidev/calculator/phoenix/MainController.java
+++ src/main/java/eu/mapidev/calculator/phoenix/MainController.java
@@ -1,7 +1,10 @@
 package eu.mapidev.calculator.phoenix;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
@@ -12,6 +15,10 @@ public class MainController implements Initializable {
 
     @FXML
     private GridPane root;
+    @FXML
+    private Label lblResult;
+    @FXML
+    private Label lblMemory;
 
     private Stage primaryStage;
 
@@ -30,5 +37,20 @@ public class MainController implements Initializable {
 
     public void setPrimaryStage(Stage primaryStage) {
         this.primaryStage = primaryStage;
+    }
+
+    @FXML
+    public void clean(ActionEvent actionEvent) {
+        lblMemory.setText("0");
+        lblResult.setText("0");
+    }
+
+    @FXML
+    public void concatNumber(ActionEvent actionEvent) {
+        String buttonTextNumber = ((Button) actionEvent.getSource()).getText();
+        if ("0".equals(lblResult.getText()))
+            lblResult.setText(buttonTextNumber);
+        else
+            lblResult.setText(lblResult.getText() + buttonTextNumber);
     }
 }
