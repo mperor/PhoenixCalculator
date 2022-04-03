7cfbe70fced4e0cd12338f33ba96739069f4a78a
Mark Pi <mpietryga93@gmail.com>
2022-04-03 13:59:13 +0200

    Add binding keys to buttons by text + remove focus traversable problem

--- src/main/java/eu/mapidev/calculator/phoenix/MainController.java
+++ src/main/java/eu/mapidev/calculator/phoenix/MainController.java
@@ -2,13 +2,16 @@ package eu.mapidev.calculator.phoenix;
 
 import eu.mapidev.calculator.phoenix.utils.ArithmeticOperation;
 import eu.mapidev.calculator.phoenix.utils.ResultUtils;
+import javafx.animation.PauseTransition;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
+import javafx.util.Duration;
 
 import java.math.BigDecimal;
 import java.net.URL;
@@ -45,6 +48,74 @@ public class MainController implements Initializable {
                 primaryStage.setY(dragEvent.getScreenY() - pressEvent.getSceneY());
             });
         });
+
+        root.setOnKeyPressed(keyEvent -> {
+            root.getChildren().stream()
+                    .filter(node -> node instanceof Button)
+                    .map(node -> (Button) node)
+                    .filter(button -> button.getText().equals(getBindingButtonTextByEvent(keyEvent)))
+                    .findFirst()
+                    .ifPresent(this::buttonFireWithClickAnimation);
+        });
+    }
+
+    private String getBindingButtonTextByEvent(KeyEvent keyEvent) {
+        switch (keyEvent.getCode()) {
+            case ENTER:
+            case EQUALS:
+                return "=";
+            case NUMBER_SIGN:
+                return "+/-";
+            case COMMA:
+            case SEPARATOR:
+            case DECIMAL:
+                return ",";
+            case DELETE:
+                return "C";
+            case PLUS:
+            case ADD:
+                return "+";
+            case MINUS:
+            case SUBTRACT:
+                return "-";
+            case MULTIPLY:
+                return "x";
+            case DIVIDE:
+                return "/";
+            case NUMPAD0:
+            case NUMPAD1:
+            case NUMPAD2:
+            case NUMPAD3:
+            case NUMPAD4:
+            case NUMPAD5:
+            case NUMPAD6:
+            case NUMPAD7:
+            case NUMPAD8:
+            case NUMPAD9:
+                String name = keyEvent.getCode().getName();
+                return name.substring(name.length() - 1);
+            case DIGIT0:
+            case DIGIT1:
+            case DIGIT2:
+            case DIGIT3:
+            case DIGIT4:
+            case DIGIT5:
+            case DIGIT6:
+            case DIGIT7:
+            case DIGIT8:
+            case DIGIT9:
+                return keyEvent.getCode().getName();
+        }
+
+        return null;
+    }
+
+    private void buttonFireWithClickAnimation(Button button) {
+        button.arm();
+        PauseTransition pause = new PauseTransition(Duration.seconds(0.1));
+        pause.setOnFinished(event -> button.disarm());
+        pause.play();
+        button.fire();
     }
 
     public void setPrimaryStage(Stage primaryStage) {
